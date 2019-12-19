4547c5c5bfbc075d638f19e0c964298ea0f77884
Francis James Franklin <fjf@alinameridon.com>
2019-12-19 21:21:57 +0000

    Don't use class member variables unnecessarily.

--- Buggy/InkTest/Timer.hh
+++ Buggy/InkTest/Timer.hh
@@ -8,20 +8,10 @@
 
 class Timer {
 private:
-  unsigned long m_previous;
-
-  int m_count_ms;
-  int m_count_10ms;
-  int m_count_tenths;
-
   bool m_stop;
 
 public:
   Timer() :
-    m_previous(0),
-    m_count_ms(0),
-    m_count_10ms(0),
-    m_count_tenths(0),
     m_stop(false)
   {
     // ...
@@ -51,6 +41,12 @@ public:
     m_stop = true;
   }
   void run() {
+    int count_ms = 0;
+    int count_10ms = 0;
+    int count_tenths = 0;
+
+    unsigned long previous_time = millis();
+
     m_stop = false;
 
     while (!m_stop) {
@@ -59,20 +55,20 @@ public:
       // our little internal real-time clock:
       unsigned long current_time = millis();
 
-      if (current_time != m_previous) {
-        ++m_previous;
+      if (current_time != previous_time) {
+        ++previous_time;
         every_milli();
 
-        if (++m_count_ms == 10) {
-          m_count_ms = 0;
+        if (++count_ms == 10) {
+          count_ms = 0;
           every_10ms();
 
-          if (++m_count_10ms == 10) {
-            m_count_10ms = 0;
-            every_tenth(m_count_tenths);
+          if (++count_10ms == 10) {
+            count_10ms = 0;
+            every_tenth(count_tenths);
 
-            if (++m_count_tenths == 10) {
-              m_count_tenths = 0;
+            if (++count_tenths == 10) {
+              count_tenths = 0;
               every_second();
             }
           }
